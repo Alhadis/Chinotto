9dadab3c6c44de4153a14b345f0746557d878abb
Alhadis <gardnerjohng@gmail.com>
2019-09-05 00:06:24 +1000

    Add tests for `.pointTo` assertion

--- test/extension-spec.js
+++ test/extension-spec.js
@@ -1,9 +1,17 @@
 "use strict";
 
 const {AssertionError} = require("chai");
+const fs   = require("fs");
+const path = require("path");
+
 
 describe("Chinotto", () => {
+	const tmp = path.join(__dirname, "tmp");
+	
+	before(() => fs.existsSync(tmp) || fs.mkdirSync(tmp));
 	before(() => require("../index.js"));
+	after(() => fs.readdirSync(tmp).forEach(file =>
+		fs.unlinkSync(path.join(tmp, file))));
 	
 	describe(".equalPath", () => {
 		it("identifies matching paths",    () => expect("/foo/bar//baz/asdf/quux/..").to.equalPath("/foo/bar/baz/asdf"));
@@ -33,6 +41,32 @@ describe("Chinotto", () => {
 		it("chains correctly", () => {
 			expect(__filename).to.existOnDisk.and.equal(__filename);
 			expect(__filename + ".nah").not.to.existOnDisk.and.equal(__filename);
+		});
+	});
+	
+	describe(".pointTo", () => {
+		it("resolves absolute symbolic links", () => {
+			const link = path.join(__dirname, "tmp", "absolute.link");
+			expect(path.isAbsolute(__dirname)).to.be.true; // Sanity check
+			fs.symlinkSync(__dirname, link);
+			expect(path.isAbsolute(fs.readlinkSync(link))).to.be.true;
+			expect(link).to.pointTo(__dirname);
+			expect(link).not.to.pointTo(__filename);
+		});
+		
+		it("resolves relative symbolic links", () => {
+			const link = path.join(__dirname, "tmp", "relative.link");
+			fs.symlinkSync(path.join("..", "..", "package.json"), link);
+			expect(path.isAbsolute(fs.readlinkSync(link))).to.be.false;
+			expect(link).to.pointTo(require.resolve("../package.json"));
+			expect(link).not.to.pointTo(path.join(__dirname, "package.json"));
+		});
+		
+		it("resolves broken symbolic links", () => {
+			const link = path.join(__dirname, "tmp", "broken.link");
+			fs.symlinkSync(__filename + ".nah", link);
+			expect(link).to.pointTo(__filename + ".nah");
+			expect(link).not.to.pointTo(__filename);
 		});
 	});
 });
