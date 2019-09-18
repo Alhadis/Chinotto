257d8221fd48e0c9e0095aa4efc86e9b8b1f814c
Alhadis <gardnerjohng@gmail.com>
2019-09-19 02:16:16 +1000

    Provide return value of `register()` as an export

--- register.js
+++ register.js
@@ -1,3 +1,5 @@
 "use strict";
 const main = ("object" === typeof global ? global : self);
 main.Chinotto = (main.Chinotto || require("./index.js")).register();
+if("object" === typeof exports && "undefined" !== typeof module)
+	module.exports = main.Chinotto;
